d4197b02ea38809e3293d5d50c78d1216552827b
Aaquib <aaquibskimbox@gmail.com>
2025-02-28 13:32:51 -0500

    feat:header web completed

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,12 +1,42 @@
+import Link from "next/link";
 import style from "@/styles/navbar.module.css";
+import { GoArrowUpRight } from "react-icons/go";
 
 export default function Header() {
   return (
-    <nav className={style.navContainer}>
+    <div className={style.navContainer} aria-label="banner">
       <div className={style.navbarContent}>
-        <div className={style.logoContainer}>Logo</div>
-        <div className={style.menuConatiner}>test</div>
+        <div className={style.logoContainer} aria-label="home">
+          Logo
+        </div>
+        <nav role="navigation" className={style.navbar}>
+          <Link className={style.navItem} href="#">
+            Home
+          </Link>
+          <Link className={style.navItem} href="#">
+            Services
+          </Link>
+          <Link className={style.navItem} href="#">
+            About
+          </Link>
+          <Link className={style.navItem} href="#">
+            FAQ
+          </Link>
+        </nav>
+        <div className={style.navCTAButton}>
+          <Link href="#" className={style.ctaLink}>
+            <div className={style.ctaText}>Contact Us Now</div>
+            <div className={style.ctaAnimation}>
+              <div className={style.ctaArrow1}>
+                <GoArrowUpRight />
+              </div>
+              <div className={style.ctaArrow2}>
+                <GoArrowUpRight />
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
